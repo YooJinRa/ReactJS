da916c8adffc1b61a6e81c35fd77d674b7b11daa
YooJinRa <rarastudy@naver.com>
2022-07-10 01:00:52 +0900

    make_coinTracker_page_nomadcorder_2200709

--- react_for_biginners_nomadcoders/src/App.js
+++ react_for_biginners_nomadcoders/src/App.js
@@ -1,37 +1,47 @@
-import {useState} from "react"
-
+import {useEffect, useState} from "react"
 
 function App() {
-  const [toDo, setToDo] = useState("")
-  const [toDos, setToDos] = useState([])
-  const onChange = (event) => setToDo(event.target.value)
-  const onSubmit = (event) => {
-    event.preventDefault()
-    if(toDo === "") {
-      return
-    }
-    setToDos((currentArray) => [toDo, ...currentArray])
-    setToDo("")
-  }
+    const [loading, setLoading] = useState(true)
+    const [coins, setCoins] = useState([])
+    const [money, setMoney] = useState("")
+    const [selectCoin, setSelectCoin] = useState([])
 
-  return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          value={toDo} 
-          onChange={onChange} 
-          type="text" 
-          placeholder="Write your to do..." 
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
-      
-    </div>
-  )
+    const onChange = (event) => {
+        setMoney(event.target.value)
+    }
+    const onChangeSelect = (event) => {
+        setSelectCoin(event.target.value)
+    }
+
+    console.log(money)
+    console.log(selectCoin)
+    console.log(money*selectCoin)
+    
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json)
+                setLoading(false)
+            })
+    }, [])
+
+    return (
+        <div>
+            <h1>The Coins! ({loading ? "" : coins.length})</h1>
+            <p>
+                <label for="money">코인으로 바꾸실 USD를 입력해주세요.</label><br />
+                <input id="money" value={money} onChange={onChange} type="number" placeholder="Write your USD..." />
+            </p>
+            {loading ? <strong>Loading...</strong> : <select onChange={onChangeSelect}>
+                {coins.map((coin) => <option value={coin.quotes.USD.price}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</option>)}
+            </select>}
+            <p>입력하신 USD로 바꿀 수 있는 코인은? <br /><strong>{money/selectCoin}</strong></p>
+            
+            
+            
+        </div>
+    )
 }
 export default App;
+
